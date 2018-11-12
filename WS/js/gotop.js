f02d018726251d7c5ca4bfaee375d74a3b6107d2
eikiychang-dell <eikiybaby@gmail.com>
2018-11-12 18:44:53 +0800

    更新網頁

--- WS/js/gotop.js
+++ WS/js/gotop.js
@@ -2,13 +2,12 @@
  $(function(){
 	$("#p01").click(function goTop(){
 		$("html,body").stop(true,false).animate({scrollTop:850},900);
-	});	
-	return false;	 
+	});
+	return false;
  });
 
 //點右邊top
  $(function(){
-	
 	$(window).scroll(function(){
 		var HEIGHT = $(window).scrollTop() + $(window).innerHeight()-150;
 		if( $(window).scrollTop() > 400 ){
@@ -16,11 +15,11 @@
 		}else{
 			$("p.go_top").stop(true,false).animate({top:-350},900);	}
 	});
-	
+
 	$("#TOP").click(function goTop(){
 		$("html,body").stop(true,false).animate({scrollTop:0},900);
 	});
-	return false;	 
+	return false
  });
 
 
@@ -32,9 +31,8 @@
 		}else{$(".goTop").fadeOut("");}
 	});
 	$('.top_go').click(function(){ $('html,body').stop(true,false).animate({scrollTop:0}, 800); }); 
-	$('p.btn a.gogogog1').click(function(){ $('html,body').animate({scrollTop:$('#section1').offset().top}, 800); }); 
-	$('p.btn a.gogogog2').click(function(){ $('html,body').animate({scrollTop:$('#section2').offset().top}, 800); }); 
-	$('p.btn a.gogogog3').click(function(){ $('html,body').animate({scrollTop:$('#section3').offset().top}, 800); }); 
-	//$('.btm02').click(function(){ $('html,body').animate({scrollTop:$('#section2').offset().top}, 800); }); 
-	return false;	 
+	$('p.btn a.gogogog1').click(function(){ $('html,body').animate({scrollTop:$('#section1').offset().top}, 800); });
+	$('p.btn a.gogogog2').click(function(){ $('html,body').animate({scrollTop:$('#section2').offset().top}, 800); });
+	$('p.btn a.gogogog3').click(function(){ $('html,body').animate({scrollTop:$('#section3').offset().top}, 800); });
+	return false;
  });
