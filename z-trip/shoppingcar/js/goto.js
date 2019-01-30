232f97c5e9dfc40a9340f40ec388e12e96921959
eikiychang-dell <eikiybaby@gmail.com>
2019-01-31 05:27:47 +0800

    Update html + css + js

--- z-trip/shoppingcar/js/goto.js
+++ z-trip/shoppingcar/js/goto.js
@@ -1,67 +1,51 @@
 $(function(){
    	$('.go00').click(function(){ $('html,body').animate({scrollTop:0},900);});
 	$('.go01').click(function(){ $('html,body').animate({scrollTop:$('#goThe01').offset().top}, 800); });
-
 	$('.inside_go01').click(function(){ $('html,body').animate({scrollTop:$('#inside_goto_01').offset().top}, 800); });
-	$('.inside_go02').click(function(){ $('html,body').animate({scrollTop:$('#inside_goto_02').offset().top}, 800); });
-	$('.inside_go03').click(function(){ $('html,body').animate({scrollTop:$('#inside_goto_03').offset().top}, 800); });
-	$('.inside_go04').click(function(){ $('html,body').animate({scrollTop:$('#inside_goto_04').offset().top}, 800); });
-	$('.inside_go05').click(function(){ $('html,body').animate({scrollTop:$('#inside_goto_05').offset().top}, 800); });
-	$('.inside_go06').click(function(){ $('html,body').animate({scrollTop:$('#inside_goto_06').offset().top}, 800); });
 	return false;
 });
 
+
+
 $(function(){
-	$("ul.menu_type li h5.has_sub").click(function(){
-	  $(this).parent().find('ul.the_sub_menu').toggle();
-	});
-	return false;
-});
-$(function(){
-	$(".for_mob_box a.more").click(function(){
-	  $(this).parent().find('.box-none').toggle();
+	$('a.goto_step2').click(function(){
+		$('#buy_list').hide();
+		$('#pay_info').hide();
+		$('#fill_info').show();
+		$('html,body').animate({scrollTop:0},900);
+		$('#top_step ul.contents_box').find('li').removeClass('btn_in');
+		$('#top_step ul.contents_box').find('li.step-2').addClass('btn_in');
 	});
 	return false;
 });
 
-// $(function(){
-// 	$( "ul.menu_type li h5" ).click(
-// 	 function() {
-// 	 	$(this).parent().find('ul.the_sub_menu').show();
-// 	    $(this).parent().addClass("open");
-// 	  }, function() {
-// 	  	$(this).parent().find('ul.the_sub_menu').hide();
-// 	    $(this).parent().removeClass("open");
-// 	  }
-// 	);
-// 	return false;
-// });
+$(function(){
+	$('a.goto_step3').click(function(){
+		$('#buy_list').hide();
+		$('#fill_info').hide();
+		$('#pay_info').show();
+		$('html,body').animate({scrollTop:0},900);
+		$('#top_step ul.contents_box').find('li').removeClass('btn_in');
+		$('#top_step ul.contents_box').find('li.step-3').addClass('btn_in');
+	});
+	return false;
+});
 
+$(function(){
+	$('.the_switch a.show_info').click(function(){
+		$(this).parent().removeClass('the_switch');
+		$(this).hide();
+		$(this).parent().find('a.hide_info').show();
+	});
+	return false;
+});
 
-// $(function(){
-//     $('ul.menu_type li h5').click(function () {
-//         if (!$(this).parent().hasClass('open')) {
-//             $(this).parent().find('ul.the_sub_menu').addClass('ul_show');
-//         } else {
-//             $(this).parent().find('ul.the_sub_menu').removeClass('ul_show');
-//         }
-//     });
-//     return false;
-// });
+$(function(){
+	$('a.hide_info').click(function(){
+		$(this).parent().addClass('the_switch');
+		$(this).hide();
+		$(this).parent().find('a.show_info').show();
+	});
+	return false;
+});
 
-
-
-
-
-// 左邊選單
-// $(function(){
-//     $('ul.menu_type li h5').click(function () {
-//         if (!$('.dLeft').hasClass('menu-left')) {
-//             $('.dLeft').addClass('menu-left');
-//             $('#rightEditBox').removeClass('rightEditBox_w100');
-//         } else {
-//             $('.dLeft').removeClass('menu-left');
-//             $('#rightEditBox').addClass('rightEditBox_w100');
-//         }
-//     });
-// });
