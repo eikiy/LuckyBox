ee524b8a23bdd5014050ae4060abc9cac1685b51
eikiychang-dell <eikiybaby@gmail.com>
2019-02-20 10:39:34 +0800

    Update : goto.js

--- JH/js/goto.js
+++ JH/js/goto.js
@@ -1,8 +1,16 @@
 $(function(){
    	$('.go00').click(function(){ $('html,body').animate({scrollTop:0},900);});
 	$('.go01').click(function(){ $('html,body').animate({scrollTop:$('#goThe01').offset().top}, 800); });
+
+	$('.inside_go01').click(function(){ $('html,body').animate({scrollTop:$('#inside_goto_01').offset().top}, 800); });
+	$('.inside_go02').click(function(){ $('html,body').animate({scrollTop:$('#inside_goto_02').offset().top}, 800); });
+	$('.inside_go03').click(function(){ $('html,body').animate({scrollTop:$('#inside_goto_03').offset().top}, 800); });
+	$('.inside_go04').click(function(){ $('html,body').animate({scrollTop:$('#inside_goto_04').offset().top}, 800); });
+	$('.inside_go05').click(function(){ $('html,body').animate({scrollTop:$('#inside_goto_05').offset().top}, 800); });
+	$('.inside_go06').click(function(){ $('html,body').animate({scrollTop:$('#inside_goto_06').offset().top}, 800); });
 	return false;
 });
+
 $(function(){
 	$("ul.menu_type li h5.has_sub").click(function(){
 	  $(this).parent().find('ul.the_sub_menu').toggle();
