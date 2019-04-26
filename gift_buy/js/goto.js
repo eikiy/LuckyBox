5c016f5bfd56dcc95a658c6e98885c1439241a67
eikiychang-dell <eikiybaby@gmail.com>
2019-04-26 18:28:47 +0800

    Update

--- gift_buy/js/goto.js
+++ gift_buy/js/goto.js
@@ -1,49 +1,43 @@
-// header 這邊跑不出來....><
+// $(function(){
+// 　$(window).load(function(){
+// 　　$(window).bind('scroll resize', function(){
+// 　　var $this = $(this);
+// 　　var $this_Top=$this.scrollTop();
+// 　　//當高度小於
+// 　　if($this_Top > 400){
+//   $('.header_box').addClass('top_header_fixed')
+// 　　　}
+// 　　　　if($this_Top < 400){
+// 　　　　$('.header_box').removeClass('top_header_fixed');
+// 　　　 }
+// 　　}).scroll();
+// 　});
+// });
+
+
+
+// side
+// function openNav() {
+// 	document.getElementById("theSidenav").style.width = "350px";
+// 	document.getElementById("Main_Contents").classList.add("for_blur");
+// 	document.getElementById("header_box").classList.add("for_blur");
+// }
+// function closeNav() {
+// 	document.getElementById("theSidenav").style.width = "0";
+// 	document.getElementById("Main_Contents").classList.remove("for_blur");
+// 	document.getElementById("header_box").classList.remove("for_blur");
+// }
+
+
+// $(function(){
+// 	$(" ul li").click(function(){
+// 	  $(this).find('.').find('.').slideToggle();
+// 	});
+// 	return false;
+// });
+
+// top
 $(function(){
-　$(window).load(function(){
-　　$(window).bind('scroll resize', function(){
-　　var $this = $(this);
-　　var $this_Top=$this.scrollTop();
-　　//當高度小於
-　　if($this_Top > 400){
-  $('.header_box').addClass('top_header_fixed')
-　　　}
-　　　　if($this_Top < 400){
-　　　　$('.header_box').removeClass('top_header_fixed');
-　　　 }
-　　}).scroll();
-　});
-});
-
-
-// animate
-$(function(){
-	new WOW().init();
-});
-
-// side menu
-function openNav() {
-	document.getElementById("theSidenav").style.width = "350px";
-	document.getElementById("Main_Contents").classList.add("for_blur");
-	document.getElementById("header_box").classList.add("for_blur");
-}
-function closeNav() {
-	document.getElementById("theSidenav").style.width = "0";
-	document.getElementById("Main_Contents").classList.remove("for_blur");
-	document.getElementById("header_box").classList.remove("for_blur");
-}
-
-// faq
-$(function(){
-	$(".faq_box ul.faq_list li").click(function(){
-	  $(this).find('.faq_info').find('.answer').slideToggle();
-	});
-	return false;
-});
-
-// QA go
-$(function(){
-	$('a.go_top').click(function(){ $('html,body').animate({scrollTop:0},900);});
 	$('.go_02').click(function(){ $('html,body').animate({scrollTop:$('#move_goto_02').offset().top - 50}, 800); });
 	return false;
 });
