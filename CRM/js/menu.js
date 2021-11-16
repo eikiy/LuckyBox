a88ffc5e9c508c21d59fcd70986d46bdd6b6de41
EikiyChang <eikiybaby@gmail.com>
2021-11-16 18:10:41 +0800

    add project

--- CRM/js/menu.js
+++ CRM/js/menu.js
@@ -5,6 +5,10 @@ $(function(){
         $(this).next(".menu").fadeToggle(0);
     return false;
     });
+    $(".main-container .filter-box a").click(function(){
+        $(this).next(".filter-list").fadeToggle(0);
+    return false;
+    });
 });
 
 // Popup
