b473e8372ea721129ed76a7a7475c3ab14a13a24
EikiyChang <eikiybaby@gmail.com>
2021-12-08 14:59:08 +0800

    新增客戶管理和日曆調整
    1.新增客戶管理頁面
    2.產品和專案新增"永久刊登"
    3.產品新增勾選 - 必選產品

--- CRM/js/menu.js
+++ CRM/js/menu.js
@@ -9,9 +9,18 @@ $(function(){
         $(this).next(".filter-list").fadeToggle(0);
     return false;
     });
+    $(".main-container .company-information ul li a.add").click(function(){
+        $(this).parent().parent().next(".menu").fadeToggle(0);
+    return false;
+    });
+    $("ul.show-contact-list li .name").click(function(){
+        $(this).next(".info").fadeToggle(0);
+        $(this).toggleClass( "arr-down" );
+    return false;
+    });
 });
 
-// Popup
+// Popupd
 $(function(){
   $('.popup-link').magnificPopup({
     removalDelay: 300,
