e941a2cae0fe090b5c32a00c1b3827275996aed2
EikiyChang <eikiybaby@gmail.com>
2023-03-26 22:34:30 +0800

    ofa77 - 更新mob tab js

---
+++ bolin/c_ofa77/js/menu.js
@@ -0,0 +1,70 @@
+// popup-link
+// $(function () {
+// 	$('.popup-link').magnificPopup({
+// 		removalDelay: 300,
+// 		mainClass: 'mfp-fade'
+// 	});
+// });
+
+// top subnav
+$(function () {
+	$(".subnav").hover(function () {
+		$(this).find(".subnav-content").fadeToggle();
+		return false;
+	});
+});
+			
+// scrollTop
+$(function(){
+	$(window).scroll(function(){
+		if( $(window).scrollTop() > 400 ){
+			$("#gotop").stop(true,false).animate({opacity:1},);
+		}else{
+			$("#gotop").stop(true,false).animate({opacity:0},);
+		}
+	});
+	$("#gotop").click(function goTop(){
+		$("html,body").stop(true,false).animate({scrollTop:0},900);
+	});
+	return false;
+});
+
+// mob tab
+$(function(){
+	$("#active-01").click(function () {
+		$("#a2-open, #a3-open, #a4-open, #a5-open, #a6-open").hide();
+		$("#active-02, #active-03, #active-04, #active-05, #active-06").removeClass("active");
+		$("#a1-open").show();
+		$(this).addClass("active");
+	});
+	$("#active-02").click(function () {
+		$("#a1-open, #a3-open, #a4-open, #a5-open, #a6-open").hide();
+		$("#active-01, #active-03, #active-04, #active-05, #active-06").removeClass("active");
+		$("#a2-open").show();
+		$(this).addClass("active");
+	});
+	$("#active-03").click(function () {
+		$("#a1-open, #a2-open, #a4-open, #a5-open, #a6-open").hide();
+		$("#active-01, #active-02, #active-04, #active-05, #active-06").removeClass("active");
+		$("#a3-open").show();
+		$(this).addClass("active");
+	});
+	$("#active-04").click(function () {
+		$("#a1-open, #a2-open, #a3-open, #a5-open, #a6-open").hide();
+		$("#active-01, #active-02, #active-03, #active-05, #active-06").removeClass("active");
+		$("#a4-open").show();
+		$(this).addClass("active");
+	});
+	$("#active-05").click(function () {
+		$("#a1-open, #a2-open, #a3-open, #a4-open, #a6-open").hide();
+		$("#active-01, #active-02, #active-03, #active-04, #active-06").removeClass("active");
+					$("#a5-open").show();
+					$(this).addClass("active");
+	});
+	$("#active-06").click(function () {
+		$("#a1-open, #a2-open, #a3-open, #a4-open, #a5-open").hide();
+		$("#active-01, #active-02, #active-03, #active-04, #active-05").removeClass("active");
+		$("#a6-open").show();
+		$(this).addClass("active");
+	});
+});
